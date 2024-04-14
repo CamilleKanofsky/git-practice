3e5a933b5f83bbc9a70f5ce746cb5c2b61b26803
CamilleKanofsky <camille@rsdiv.com>
2024-04-14 15:28:37 -0400

    my calculator files

--- src/cal2java.java
+++ src/cal2java.java
@@ -1,5 +1,8 @@
-public class cal2java
-public class Main {
+
+
+public  class cal2java {
+
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -10,4 +13,8 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        System.out.println("hi camille");
+
     }
+
+}
