603ba735dac31324d228d71ef81cf85255f0b5a6
CamilleKanofsky <camille@rsdiv.com>
2024-04-21 12:10:04 -0400

    add renamed file

--- src/subtraction2.java
+++ src/subtraction2.java
@@ -1,4 +1,4 @@
-public class subtraction {
+public class subtraction2 {
     public static void main(String[] args) {
             //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
             // to see how IntelliJ IDEA suggests fixing it.
